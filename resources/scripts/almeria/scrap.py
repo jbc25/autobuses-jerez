8f4bb3e6bf5222c9330b125b46f348c01b02d263
julio <jberzal86@gmail.com>
2021-03-06 20:11:39 +0100

    - script almeria retrieving all basic data

--- resources/scripts/almeria/scrap.py
+++ resources/scripts/almeria/scrap.py
@@ -2,6 +2,8 @@ from lxml import etree, html
 import json
 import requests
 import pyperclip
+import urllib.parse
+import re
 
 
 
@@ -35,21 +37,53 @@ for line_node in root.xpath("//div[@class='links']/a[@class='lines']"):
 	bus_stops_html = response_bus_stops.text
 	root_bus_stops = html.fromstring(bus_stops_html)
 
+	# Extract wat time codes
+	script_code = root_bus_stops.xpath("string(//script[@type='text/javascript']/text())")
+
+	configure_button_lines = re.findall('ConfigureButton(.*);', script_code)
+
+	bus_line_waitTimeCode = ''
+	bus_stops_waitTimeCode = {}
+
+	for configure_button_line in configure_button_lines:
+		configure_button_line = re.sub('[\(\)\"\s]', '', configure_button_line)
+		line_parts = configure_button_line.split(',')
+		bus_line_waitTimeCode = line_parts[1]
+		bus_stops_waitTimeCode[line_parts[0]] = line_parts[2]
+
 	
 	for bus_stop_node in root_bus_stops.xpath("//li[@class='waitTimeBusStop']"): 
 
-		code = bus_stop_node.xpath("string(a/span/span[@class='label']/text())")
-		name = bus_stop_node.xpath("string(div/span[@class='name']/text())")
+		button_name = bus_stop_node.attrib.get('id')
+
+		bus_stop_waitTimeCode = bus_stops_waitTimeCode[button_name]
+
+		bus_stop_code = bus_stop_node.xpath("string(a/span/span[@class='label']/text())")
+		bus_stop_name = bus_stop_node.xpath("string(div/span[@class='name']/text())")
+
+		print(f'Retrieving bus stop info. Code: {bus_stop_code}')
+
+		response_bus_stop = requests.get(f'{url_base}/tiempos-de-espera/parada/{bus_stop_code}')
+		root_bus_stop = html.fromstring(response_bus_stop.text)
+		link_google_maps = root_bus_stop.xpath("string(//a[@class='openMap']/@href)")
+		query_string = urllib.parse.urlparse(link_google_maps).query
+		params = dict(urllib.parse.parse_qsl(query_string))
+		coords_inverted = params['destination'].split(",")
+		bus_stop_coords = [float(coords_inverted[1]), float(coords_inverted[0])]
+
+
+
 		bus_stops.append(
 			{
-				'code': code,
-				'name': name,
+				'code': bus_stop_code,
+				'name': bus_stop_name,
 				'lineId': line_number,
-				'waitTimeCode': '',
-				'coordinates': [0, 0],
+				'waitTimeCode': bus_stop_waitTimeCode,
+				'coordinates': bus_stop_coords,
 			}
 		)
 
+		
 	
 	bus_line = {
 		'id': line_number,
@@ -57,7 +91,7 @@ for line_node in root.xpath("//div[@class='links']/a[@class='lines']"):
 		'name': f'Línea {line_number}',
 		'description': line_description,
 		'busStops': bus_stops,
-		'waitTimeCode': '',
+		'waitTimeCode': bus_line_waitTimeCode,
 		'path': [[0, 0], [1, 1]],
 	}
 
@@ -65,10 +99,10 @@ for line_node in root.xpath("//div[@class='links']/a[@class='lines']"):
 	report['count'] += 1
 
 	# REMOVE THIS FOR SCRAP ALL LINES
-	if report['count'] == 3:
-		break
+	#if report['count'] == 1:
+	#	break
 
-print(f'\n\nReport: {json.dumps(report, indent=4)}')
+#print(f'\n\nReport: {json.dumps(report, indent=4)}')
 
 data_formatted = json.dumps(bus_data, indent=4)
 
