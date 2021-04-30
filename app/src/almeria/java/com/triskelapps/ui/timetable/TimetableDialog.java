19365e3281fb3b2ccda9228204114dd28720938b
julio <jberzal86@gmail.com>
2021-04-30 18:37:49 +0200

    - adapting flavors

---
+++ app/src/almeria/java/com/triskelapps/ui/timetable/TimetableDialog.java
@@ -0,0 +1,206 @@
+package com.triskelapps.ui.timetable;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.triskelapps.R;
+import com.triskelapps.base.BaseActivity;
+import com.triskelapps.base.BaseInteractor;
+import com.triskelapps.databinding.DialogTimetableBinding;
+import com.triskelapps.interactor.TimetableInteractor;
+import com.triskelapps.model.BusStop;
+import com.triskelapps.util.CountlyUtil;
+import com.triskelapps.util.DateUtils;
+
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.HttpUrl;
+
+public class TimetableDialog extends DialogFragment implements WebView.FindListener {
+
+    private static final String ARG_BUS_STOP = "arg_bus_stop";
+    private static final String TAG = "TimetableDialog";
+    private DialogTimetableBinding binding;
+    private BusStop busStop;
+    private String infoHtml;
+    private String dayType;
+
+    public static TimetableDialog createDialog(BusStop busStop) {
+        TimetableDialog timetableDialog = new TimetableDialog();
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_BUS_STOP, busStop);
+        timetableDialog.setArguments(args);
+        return timetableDialog;
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        busStop = (BusStop) getArguments().getSerializable(ARG_BUS_STOP);
+
+        binding = DialogTimetableBinding.inflate(getActivity().getLayoutInflater());
+
+        binding.webviewTimetable.setFindListener(this);
+
+        showProgressBar();
+
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+
+
+
+        URL url = new HttpUrl.Builder()
+                .scheme("https")
+                .host("www.surbusalmeria.es")
+                .addPathSegments("API/GetWaitTime")
+                .addQueryParameter("l", busStop.)
+                .build().url();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null,
+                        response -> processResponseJson(response),
+                        error -> error.printStackTrace());
+
+
+        jsonObjectRequest.setShouldCache(false);
+
+        queue.add(jsonObjectRequest);
+
+        new TimetableInteractor(getActivity(), null)
+                .getTimetable(busStop.getLineId(), busStop.getCode(), new BaseInteractor.CallbackPost() {
+                    @Override
+                    public void onSuccess(String body) {
+                        if (getActivity() == null) {
+                            return;
+                        }
+                        infoHtml = body;
+                        loadTimetableHtml();
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        if (getActivity() == null) {
+                            return;
+                        }
+                        hideProgressBar();
+                        ((BaseActivity) getActivity()).toast(getString(R.string.error_loading_timetable));
+
+                        CountlyUtil.timetableError(busStop);
+                    }
+                });
+    }
+
+    private void showProgressBar() {
+        binding.progressbarWebview.setVisibility(View.VISIBLE);
+        binding.webviewTimetable.setVisibility(View.GONE);
+    }
+
+    private void hideProgressBar() {
+        binding.progressbarWebview.setVisibility(View.GONE);
+        binding.webviewTimetable.setVisibility(View.VISIBLE);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        showHeaderInfo();
+    }
+
+    private void showHeaderInfo() {
+
+        String time = DateUtils.formatTime.format(new Date());
+        dayType = DayTypeUtil.with(getContext()).getDayType();
+        String infoText = getString(R.string.info_timetable_format, busStop.getLineId(), busStop.getName(), dayType, time);
+        binding.tvTimetableInfo.setText(infoText);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(binding.getRoot())
+                .setNegativeButton(R.string.back, (dialog, which) -> dismiss())
+                .create();
+    }
+
+    private void loadTimetableHtml() {
+
+        if (getContext() != null && infoHtml != null) {
+
+            binding.webviewTimetable.loadDataWithBaseURL(null, infoHtml, "text/html", "utf-8", null);
+
+            binding.webviewTimetable.setWebViewClient(new WebViewClient() {
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    super.onPageFinished(view, url);
+
+                    if (getActivity() == null) {
+                        CountlyUtil.recordHandledException(new Exception("Error timetable No Context"));
+                        return;
+                    }
+
+                    hideProgressBar();
+
+                    // Workaround to make this work in Android 9
+                    new Handler().postDelayed(() -> {
+
+                        if (getActivity() == null) {
+                            CountlyUtil.recordHandledException(new Exception("Error timetable No Context"));
+                            return;
+                        }
+
+                        if (dayType != null) {
+                            String dayTypeWebName = convertDayType(dayType);
+                            binding.webviewTimetable.findAllAsync(dayTypeWebName);
+                        }
+                    }, 50);
+
+                }
+
+
+            });
+        }
+    }
+
+    private String convertDayType(String dayType) {
+        if (TextUtils.equals(dayType, getString(R.string.sunday)) || dayType.contains(getString(R.string.festive))) {
+            return "DOMINGOS Y FESTIVOS";
+        } else if (TextUtils.equals(dayType, getString(R.string.saturday))) {
+            return "SÁBADOS";
+        } else {
+            return "LABORABLES";
+        }
+    }
+
+
+    @Override
+    public void onFindResultReceived(int activeMatchOrdinal, int numberOfMatches, boolean isDoneCounting) {
+        if (isDoneCounting && numberOfMatches > 0) {
+            binding.webviewTimetable.findNext(true);
+            binding.webviewTimetable.clearMatches();
+        }
+
+    }
+
+}
--- app/src/main/java/com/triskelapps/ui/main/bus_stops/BusStopsFragment.java
+++ app/src/main/java/com/triskelapps/ui/main/bus_stops/BusStopsFragment.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.triskelapps.App;
-import com.triskelapps.BuildConfig;
 import com.triskelapps.R;
 import com.triskelapps.base.BaseMainFragment;
 import com.triskelapps.databinding.FragmentBusStopsBinding;
