d1ef910e5159d281740d53a37f1a78e2dc9fd915
julio <jberzal86@gmail.com>
2021-06-09 18:48:45 +0200

    - new icons

--- app/src/almeria/java/com/triskelapps/CityData.java
+++ app/src/almeria/java/com/triskelapps/CityData.java
@@ -8,10 +8,6 @@ public class CityData {
     public final static LatLng COORD_CENTER = new LatLng(36.84646059164273, -2.4473888073442174);
     public final static float INITIAL_ZOOM = 12.5f;
 
-    public final static LatLng COORD_NORTH_EAST = new LatLng(36.707457, -6.093387);
-    public final static LatLng COORD_SOUTH_WEST = new LatLng(36.663924, -6.160751);
-
-
     public final static LatLng AUTOCOMPLETE_RESULTS_COORDS_NORTH_EAST = new LatLng(36.88516389679643, -2.2785760332076155);
     public final static LatLng AUTOCOMPLETE_RESULTS_COORDS_SOUTH_WEST = new LatLng(36.81285394888109, -2.497121070841281);
 
