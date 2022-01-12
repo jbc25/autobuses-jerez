d1484c343726a56d1540e8d22e5b4216b84b0bcc
julio <jberzal86@gmail.com>
2022-01-12 12:02:51 +0100

    - updated jerez and almeria festive days

--- app/src/jerez/java/com/triskelapps/ui/timetable/FestiveDays.java
+++ app/src/jerez/java/com/triskelapps/ui/timetable/FestiveDays.java
@@ -13,21 +13,21 @@ public class FestiveDays {
 
         Map<String, String> festiveDaysMap = new HashMap<>();
 
-        festiveDaysMap.put("01/01/2021", context.getString(R.string.festive_1_new_year)); // nacional
-        festiveDaysMap.put("06/01/2021", context.getString(R.string.festive_2_magic_kings)); // nacional
-        festiveDaysMap.put("01/03/2021", context.getString(R.string.festive_3_andalucia_day) + " " + context.getString(R.string.moved)); // regional
-        festiveDaysMap.put("01/04/2021", context.getString(R.string.festive_4_jueves_santo)); // regional
-        festiveDaysMap.put("02/04/2021", context.getString(R.string.festive_5_viernes_santo)); // nacional
-        festiveDaysMap.put("01/05/2021", context.getString(R.string.festive_6_worker_day)); // nacional
-        festiveDaysMap.put("11/05/2021", context.getString(R.string.festive_7_feria_caballo)); // local
-        festiveDaysMap.put("16/08/2021", context.getString(R.string.festive_8_asuncion_virgen) + " " + context.getString(R.string.moved)); // regional
-        festiveDaysMap.put("24/09/2021", context.getString(R.string.festive_9_virgen_merced)); // local
-        festiveDaysMap.put("11/10/2021", context.getString(R.string.festive_91_san_dionisio) + " " + context.getString(R.string.moved)); // local
-        festiveDaysMap.put("12/10/2021", context.getString(R.string.festive_10_nacional_day)); // nacional
-        festiveDaysMap.put("01/11/2021", context.getString(R.string.festive_11_all_saints)); // nacional
-        festiveDaysMap.put("06/12/2021", context.getString(R.string.festive_12_constitution_day)); // nacional
-        festiveDaysMap.put("08/12/2021", context.getString(R.string.festive_13_inmaculada_day)); // nacional
-        festiveDaysMap.put("25/12/2021", context.getString(R.string.festive_14_christmas)); // nacional
+        festiveDaysMap.put("01/01/2022", context.getString(R.string.festive_1_new_year)); // nacional
+        festiveDaysMap.put("06/01/2022", context.getString(R.string.festive_2_magic_kings)); // nacional
+        festiveDaysMap.put("28/02/2022", context.getString(R.string.festive_3_andalucia_day)); // regional
+        festiveDaysMap.put("14/04/2022", context.getString(R.string.festive_4_jueves_santo)); // regional
+        festiveDaysMap.put("15/04/2022", context.getString(R.string.festive_5_viernes_santo)); // nacional
+        festiveDaysMap.put("02/05/2022", context.getString(R.string.festive_6_worker_day) + " " + context.getString(R.string.moved)); // nacional
+        festiveDaysMap.put("09/05/2022", context.getString(R.string.festive_7_feria_caballo)); // local
+        festiveDaysMap.put("15/08/2022", context.getString(R.string.festive_8_asuncion_virgen)); // regional
+        festiveDaysMap.put("24/09/2022", context.getString(R.string.festive_9_virgen_merced)); // local
+//        festiveDaysMap.put("11/10/2022", context.getString(R.string.festive_91_san_dionisio) + " " + context.getString(R.string.moved)); // local
+        festiveDaysMap.put("12/10/2022", context.getString(R.string.festive_10_nacional_day)); // nacional
+        festiveDaysMap.put("01/11/2022", context.getString(R.string.festive_11_all_saints)); // nacional
+        festiveDaysMap.put("06/12/2022", context.getString(R.string.festive_12_constitution_day)); // nacional
+        festiveDaysMap.put("08/12/2022", context.getString(R.string.festive_13_inmaculada_day)); // nacional
+        festiveDaysMap.put("26/12/2022", context.getString(R.string.festive_14_christmas) + " " + context.getString(R.string.moved)); // nacional
 
         return festiveDaysMap;
     }
