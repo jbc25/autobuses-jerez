24a7b22d2bda872d4bc457f8060548707aef6fbe
julio <jberzal86@gmail.com>
2023-06-15 14:30:41 +0200

    - Gradle upgraded - starting find nearby lines feature

--- app/src/main/java/com/triskelapps/ui/main/address/AddressFragment.java
+++ app/src/main/java/com/triskelapps/ui/main/address/AddressFragment.java
@@ -11,16 +11,23 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.model.RectangularBounds;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.maps.android.SphericalUtil;
+import com.triskelapps.App;
 import com.triskelapps.CityData;
 import com.triskelapps.R;
 import com.triskelapps.base.BaseMainFragment;
 import com.triskelapps.databinding.FragmentAddressBinding;
+import com.triskelapps.model.BusLine;
 import com.triskelapps.util.CountlyUtil;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -57,7 +64,7 @@ public class AddressFragment extends BaseMainFragment {
 
         autocompleteFragment = (CustomPlaceAutocompleteFragment) getChildFragmentManager().findFragmentById(R.id.fragment_places_autocomplete);
 
-        autocompleteFragment.setCountry("ES");
+        autocompleteFragment.setCountries("ES");
 
         // https://developers.google.com/places/android-sdk/autocomplete#constrain-AC-results
         autocompleteFragment.setLocationRestriction(RectangularBounds.newInstance(
@@ -72,6 +79,7 @@ public class AddressFragment extends BaseMainFragment {
             public void onPlaceSelected(Place place) {
                 Log.i(TAG, "Autocomplete Places. Selected: " + place.getName());
                 getMainPresenter().onPlaceSelected(place);
+                findNearbyLines(place);
                 CountlyUtil.selectPlace(place.getName());
             }
 
@@ -83,6 +91,27 @@ public class AddressFragment extends BaseMainFragment {
         });
 
         autocompleteFragment.setClearRunnable(() -> getMainPresenter().onClearPlaceAutocompleteText());
+    }
+
+    private void findNearbyLines(@NonNull Place place) {
+
+        double distanceLimit = 100; // meters
+
+        List<BusLine> busLines = App.getBusLinesData(getActivity());
+        for (BusLine busLine : busLines) {
+            List<LatLng> path = busLine.getPath().stream().map(coords ->
+                    new LatLng(coords.get(0), coords.get(1))).collect(Collectors.toList());
+
+            // Verifica si la polilínea está cerca del punto de referencia
+            for (LatLng punto : path) {
+                double distance = SphericalUtil.computeDistanceBetween(place.getLatLng(), punto);
+
+                if (distance <= distanceLimit) {
+                    Log.d(TAG, "findNearbyLines: linea: " + busLine.getId());
+                    break; // Opcional: si solo te interesa saber si hay una polilínea cercana, puedes detener la iteración aquí
+                }
+            }
+        }
     }
 
 
